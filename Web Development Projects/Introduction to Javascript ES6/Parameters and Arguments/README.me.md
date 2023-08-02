19483910786ad2cd7a84a6e4e8a1c2ea1b8e040f
Xertiem <31804681+Xertiem@users.noreply.github.com>
2023-08-02 20:19:24 +0200

    index.js + README.md
    Exercise on the second type of form of a function, with parameters and arguments

---
+++ Web Development Projects/Introduction to Javascript ES6/Parameters and Arguments/README.me.md
@@ -0,0 +1,5 @@
+# Functions Part 2
+
+## Parameters and Arguments
+
+Getting my robot to buy milk from the store and back with based on the cost of the milk.
---
+++ Web Development Projects/Introduction to Javascript ES6/Parameters and Arguments/index.js
@@ -0,0 +1,26 @@
+function getMilk(money) {
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("How much does a bottle of milk cost?");
+  console.log("It costs $1.5");
+  const bottles = Math.floor(money / 1.5);
+  console.log("buy " + bottles + " bottles of milk.");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
+
+getMilk(3);
