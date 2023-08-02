32f88e566800409a709b2ed9fc72599c940dbe6f
Xertiem <31804681+Xertiem@users.noreply.github.com>
2023-08-02 21:11:49 +0200

    Update index.js
    Added the function for the follow up exercise

--- Web Development Projects/Introduction to Javascript ES6/Parameters and Arguments/index.js
+++ Web Development Projects/Introduction to Javascript ES6/Parameters and Arguments/index.js
@@ -24,3 +24,29 @@ function getMilk(money) {
 }
 
 getMilk(3);
+
+// Another function for the exercise.
+
+function lifeInWeeks(age) {
+  /************Don't change the code above************/
+
+  //Write your code here.
+
+  const days = 90 * 365 - age * 365;
+  const weeks = 90 * 52 - age * 52;
+  const months = 90 * 12 - age * 12;
+
+  console.log(
+    "You have " +
+      days +
+      " days, " +
+      weeks +
+      " weeks, and " +
+      months +
+      " months left."
+  );
+
+  /*************Don't change the code below**********/
+}
+
+lifeInWeeks(56);
