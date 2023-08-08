4508e9ca51a6017c2e1c72d715339b24e5e7ac14
Xertiem <31804681+Xertiem@users.noreply.github.com>
2023-08-08 18:07:00 +0200

    html + javascrip files
    created a random number generator . A "love" calculator that does not take the names into consideration

---
+++ Web Development Projects/Intermediate Javascript/Random Number Generator/index.js
@@ -0,0 +1,8 @@
+var yourName = prompt("What is your name? ");
+var theirName = prompt("What is your crush's name? ");
+
+var n = Math.random();
+n = n * 100;
+n = Math.floor(n) + 1;
+
+alert("You are " + n + "% compatible.");
