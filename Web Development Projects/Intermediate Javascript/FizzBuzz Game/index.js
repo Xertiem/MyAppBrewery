6c471b0b24015bfba3163e28f3f95e2546451500
Xertiem <31804681+Xertiem@users.noreply.github.com>
2023-08-12 12:27:51 +0200

    Update index.js
    Took it a bit further now we can call fizzBuzz() in the console and it will count + add numbers to the array and if it is divisible by 3 and/or 5 it will replace the number by the appropriate keyword.

--- Web Development Projects/Intermediate Javascript/FizzBuzz Game/index.js
+++ Web Development Projects/Intermediate Javascript/FizzBuzz Game/index.js
@@ -1,6 +1,6 @@
 // My take on it where it asks the user for a number.
 
-var yourNumber = prompt("Type a number? ");
+/*var yourNumber = prompt("Type a number? ");
 
 if (yourNumber % 3 === 0) {
   if (yourNumber % 5 === 0) {
@@ -12,4 +12,25 @@ if (yourNumber % 3 === 0) {
   alert("Buzz");
 } else {
   alert("Keep counting");
+}*/
+
+//Another version of it where it counts for itself. Runs by calling fizzBuzz() in the console.
+
+var output = [];
+var count = 1;
+
+function fizzBuzz() {
+  if (count % 3 === 0) {
+    if (count % 5 === 0) {
+      output.push("FizzBuzz");
+    } else {
+      output.push("Fizz");
+    }
+  } else if (count % 5 === 0) {
+    output.push("Buzz");
+  } else {
+    output.push(count);
+  }
+  count++;
+  console.log(output);
 }
